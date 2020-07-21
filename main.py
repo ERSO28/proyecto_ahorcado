2cf4168dd526ffecfa0a5e32ffb71f65d1ae7fac
ERSO28 <riv.val.ale@gmail.com>
2020-07-21 13:11:27 -0400

    Se crea el directorio 'funciones' con la funcion de limpiar consola y se implementa el while para re utilizar el codigo.

--- main.py
+++ main.py
@@ -1,49 +1,50 @@
+from funciones import limpiador_consola
 import random
-import os
 
 
-def limpiador():
-    if os.name == 'nt':
-        os.system("cls")
-    else:
-        os.system('clear')
-
-
-palabra_secreta = random.choice(['cesped', 'sol', 'pasto', 'python'])
-palabra_oculta = '-' * len(palabra_secreta)
-errores = 0
-bandera = True
-letras_usuario = set()
-
+switch = True
 print('AHORCADO')
-while errores < 8:
-    print(palabra_oculta)
-    # print(palabra_secreta)
-    if palabra_oculta in palabra_secreta:
-        print('Lo lograste!')
+while switch:
+    palabra_secreta = random.choice(['cesped', 'sol', 'pasto', 'python'])
+    palabra_oculta = '-' * len(palabra_secreta)
+    errores = 0
+    letras_usuario = set()
+    while errores < 8:
+        print(palabra_oculta)
+        # print(palabra_secreta)
+        if palabra_oculta in palabra_secreta:
+            print('Lo lograste!')
+            break
+        else:
+            letra = input('Ingresa una letra: ')
+            limpiador_consola.limpiador()
+            if len(letra) != 1:
+                print('Debes ingresar una sola letra.')
+            elif not (letra.isalpha()):
+                print('Introduce una letra del alfabeto.')
+            else:
+                if letra in letras_usuario:
+                    print('Ya ingresaste esta letra.')
+                elif letra not in palabra_secreta:
+                    errores += 1
+                else:
+                    for i in range(len(palabra_secreta)):
+                        if letra == palabra_secreta[i]:
+                            palabra_listada = list(palabra_oculta)
+                            palabra_listada[i] = letra
+                            palabra_oculta = ''.join(palabra_listada)
+            letras_usuario.add(letra)
+    else:
+        print('Se acabaron los intentos')
+    usuario_pass = input('Desea seguir jugando?. (s/n)').lower()
+    if usuario_pass == 's':
+        switch = True
+    elif usuario_pass == 'n':
+        switch = False
+        print('Gracias por haber jugado.')
         break
     else:
-        letra = input('Ingresa una letra: ')
-        limpiador()
-        if len(letra) != 1:
-            print('Debes ingresar una sola letra.')
-        elif not (letra.isalpha()):
-            print('Introduce una letra del alfabeto.')
-        else:
-            if letra in letras_usuario:
-                print('Ya ingresaste esta letra.')
-            elif letra not in palabra_secreta:
-                errores += 1
-            else:
-                for i in range(len(palabra_secreta)):
-                    if letra == palabra_secreta[i]:
-                        palabra_listada = list(palabra_oculta)
-                        palabra_listada[i] = letra
-                        palabra_oculta = ''.join(palabra_listada)
-        letras_usuario.add(letra)
-else:
-    print('Se acabaron los intentos')
+        usuario_pass = input('Introduzca un caracter valido.')
 
-# TODO: Implementar "While" para volver a inciar programa.
-# TODO: Agregar diccionario de palabras.
-# TODO: Agregar funcion para diseño de muñecos.
+    # TODO: Agregar diccionario de palabras.
+    # TODO: Agregar funcion para diseño de muñecos.
